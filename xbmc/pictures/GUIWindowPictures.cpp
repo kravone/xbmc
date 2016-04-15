014d320cde8c40f6add0e57256d1ab0ff32ba918
Garrett Brown <themagnificentmrb@gmail.com>
2016-04-15 07:14:29 -0700

    [pictures] cosmetic: improve readability of start folder check

--- xbmc/pictures/GUIWindowPictures.cpp
+++ xbmc/pictures/GUIWindowPictures.cpp
@@ -601,9 +601,11 @@ void CGUIWindowPictures::OnItemInfo(int itemNumber)
 
 std::string CGUIWindowPictures::GetStartFolder(const std::string &dir)
 {
-  std::string lower(dir); StringUtils::ToLower(lower);
-  if (lower == "plugins" || lower == "addons")
+  if (StringUtils::EqualsNoCase(dir, "plugins") ||
+      StringUtils::EqualsNoCase(dir, "addons"))
+  {
     return "addons://sources/image/";
+  }
 
   SetupShares();
   VECSOURCES shares;
